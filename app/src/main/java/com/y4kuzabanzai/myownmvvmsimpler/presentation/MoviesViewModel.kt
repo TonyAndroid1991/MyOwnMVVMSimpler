d1073cbbddd36b83bd3a0e2b29501d7b5afe1011
Y4kuza <jantoniosanchez1991@gmail.com>
2021-03-12 12:28:44 +0100

    added activities and viewmodels

--- app/src/main/java/com/y4kuzabanzai/myownmvvmsimpler/presentation/MoviesViewModel.kt
+++ app/src/main/java/com/y4kuzabanzai/myownmvvmsimpler/presentation/MoviesViewModel.kt
@@ -4,7 +4,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.y4kuzabanzai.myownmvvmsimpler.domain.usecases.GetMoviesUseCase
 
-/**En arquitectura clean los viewmodels dependen totalmente de los caos de uso, en este caso seria GetMoviesUseCase*/
+/**En arquitectura clean los viewmodels dependen totalmente de los casos de uso, en este caso seria GetMoviesUseCase
+ * estos casos de uso seran inyectados segun tengo entendido*/
 class MoviesViewModel(private val getMoviesUseCase: GetMoviesUseCase) : ViewModel() {
 
     fun getMovies() = liveData {
